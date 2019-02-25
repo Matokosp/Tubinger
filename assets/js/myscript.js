5b56c23c72ff886b386309a7bfb464e5b2230f43
mati <jmsalinas@uc.cl>
2019-02-25 16:18:16 -0300

    update menu

---
+++ assets/js/myscript.js
@@ -0,0 +1,25 @@
+// MENU
+
+	var menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
+
+	function desplazar (){
+		$('.menu__responsive--hidden').toggleClass('show');
+		$('#hamburger').toggleClass('hamburguer-slide');
+	}
+
+	$('#hamburger').on('click', function(e){		
+		desplazar();
+		e.stopPropagation();
+		menuShow = $('.menu__responsive').children(".menu__responsive--hidden").hasClass('show');
+		console.log(menuShow)
+	})
+   
+	window.addEventListener('click', function(e){
+		// var menuHide = $('.menu-hide');
+		if (menuShow === true && e.target.className != 'menu__responsive--hidden show') {
+			// desplazar();
+			$('.menu__responsive--hidden').removeClass('show');
+			$('#hamburger').removeClass('hamburguer-slide');
+		}		
+		console.log(e.target)
+	})
