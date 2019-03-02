a1634b367cb60be5fdc794f66b5669270d2f75a9
mati <jmsalinas@uc.cl>
2019-03-01 21:12:26 -0300

    menu update script

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -24,6 +24,36 @@
 		console.log(e.target)
 	})
 
+// MENU STYLE
+
+// if (document.location.pathname == "about.html") {
+//     console.log(document.URL);
+// }
+	var sectionOne = $('#section1').offset();
+	var sectionTwo = $('#section2').offset();
+	var position = $(window).scrollTop(); 
+
+	// should start at 0
+
+	$(window).scroll(function() {
+	    var scroll = $(window).scrollTop();
+	    if( scroll > position ) {
+	        console.log('scrollDown');
+	        $('.menu__responsive').removeClass('menu__responsive-show');
+	        $('.menu__responsive').addClass('menu__responsive-hide')
+	    }
+
+	    if( scroll < position ) {
+	    	$('.menu__responsive').removeClass('menu__responsive-hide')
+	    	$('.menu__responsive').addClass('menu__responsive-show');
+	        console.log('scrollUp');	        
+	    }
+	    // else {
+	    //      console.log('scrollUp');	         
+	    // }
+	    position = scroll;
+	});
+
 
 // BEER POP UP
 
